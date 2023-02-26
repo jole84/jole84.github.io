356b09ffc403011271ab07cad221b4e2a1b8cacf
jole84 <johanlennartsson84@gmail.com>
2023-02-26 10:37:20 +0100

    Update config.js

--- brouter-web/config.js
+++ brouter-web/config.js
@@ -1,8 +1,7 @@
 (function () {
-    var hostname = window.location.hostname;
-    var origin = window.location.protocol + '//' + hostname + (window.location.port ? ':' + window.location.port : '');
 
     BR.conf = {};
+    BR.conf.host = 'https://brouter.de/';
 
     // Switch for intermodal routing demo
     BR.conf.transit = false;
@@ -12,23 +11,10 @@
     //var params = new URLSearchParams(window.location.search.slice(1));
     //BR.conf.transit = params.has('transit') && (params.get('transit') === 'true');
 
-    if (hostname.endsWith('brouter.de')) {
-        // online service (brouter.de) configuration
 
-        BR.conf.host = origin;
-        BR.conf.profilesUrl = origin + '/brouter/profiles2/';
-    } else {
-        // desktop configuration
 
-        BR.conf.host = 'https://brouter.de/';
 
-        // Pre-loading selected profile disabled locally. Needs brouter-web to run on a
-        // local web server with the profiles in a subdirectory or allowing file access
-        // in the Browser (security!), see
-        // https://github.com/mrdoob/three.js/wiki/How-to-run-things-locally
-        BR.conf.profilesUrl = 'https://jole84.github.io/profiles2/';
-        //BR.conf.profilesUrl = 'file://YOUR_PATH_TO/profiles2/';
-    }
+    BR.conf.profilesUrl = 'https://jole84.github.io/profiles2/';
 
     BR.conf.privacyPolicyUrl = '/privacypolicy.html';
 
