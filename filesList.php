5b6311c502779bfcc9b0ce5a430478435d7e3f23
jole84 <johanlennartsson84@gmail.com>
2025-05-31 16:44:25 +0200

    filesList

--- filesList.php
+++ filesList.php
@@ -1,9 +1,11 @@
 <?php
+echo(header("Access-Control-Allow-Origin: *"));
+echo(header("Access-Control-Allow-Headers: *"));
 header('Content-Type: application/json');
 $filesList = glob("rutter/*.{gpx,kml,geojson,GPX,KML,GEOJSON}", GLOB_BRACE);
 
 foreach ($filesList as $k=>$v) {
-    $filesList[$k] = str_replace("rutter/", "", $v);
+    $filesList[$k] = "https://jole84.se/" . $v;
 }
 
 usort($filesList, 'strnatcasecmp');
