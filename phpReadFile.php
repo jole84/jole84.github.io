51b2288ef11c07c272e6a6366c7eaf6f3e550202
jole84 <johanlennartsson84@gmail.com>
2025-05-31 13:50:11 +0200

    Update phpReadFile.php

--- phpReadFile.php
+++ phpReadFile.php
@@ -1,6 +1,8 @@
 <?php
-$url = $_REQUEST["url"];
+echo(header("Access-Control-Allow-Origin: *"));
+echo(header("Access-Control-Allow-Headers: *"));
 echo(header('content-type: text/txt'));
+$url = $_REQUEST["url"];
 $content = file_get_contents($url);
 echo $content;
 ?> 
